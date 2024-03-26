d673b43c970d017c46f1a4ae9811acfdd83b2035
Artem Kudryavtsev <artem.kdr@gmail.com>
2024-03-26 09:47:24 +0000

    hot fixes

--- artem-cv/src/chatbot/ActionProvider.js
+++ artem-cv/src/chatbot/ActionProvider.js
@@ -10,20 +10,21 @@ const ActionProvider = ({ createChatBotMessage, setState, children }) => {
     try {
         const rsp = await fetch('//cv.artem.work/api/chatbot?q=' + message);
         if (rsp.ok) {
-            answer = (await rsp.text())?.replace(/\n/g, '<br/>');
-            // if the server responds with input/output models, then replace it with generic message
-            if (answer?.indexOf("input:") >= 0 && answer?.indexOf("output:") >= 0) {
-              answer = i18n.t("Generic");
-            }
+            answer = (await rsp.text()) ?? "";
         }
         //console.log(answer);
     } catch (ex) {
-        answer = i18n.t("Error");
+        answer = i18n.t("Chatbot.Error");
     }
-    const botMessage = createChatBotMessage(answer + "");
+    // if the server responds with input/output models, then replace it with generic message
+    if (answer.indexOf("input:") >= 0 && answer.indexOf("output:") >= 0) {
+      answer = i18n.t("Chatbot.Generic");
+    }
+    const botMessages = answer.split('\n').map((msg) => createChatBotMessage(msg));
     setState((prev) => {
-        const newPrevMsg = prev.messages.slice(0, -1)
-        return { ...prev, messages: [...newPrevMsg, botMessage]};
+        const prevMsgLoading = prev.messages[prev.messages.length - 1]?.message === '' && prev.messages[prev.messages.length - 1]?.type === 'bot';
+        const newPrevMsg = !prevMsgLoading ? prev.messages : prev.messages.slice(0, -1);
+        return { ...prev, messages: [...newPrevMsg, ...botMessages]};
     });
   };
 
