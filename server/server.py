12ce1a76edee82f9a7d30fb5a927477ad0f3cf75
Artem Kudryavtsev <artem.kdr@gmail.com>
2024-03-27 21:49:48 +0100

    chatbot update and small fixes

--- server/server.py
+++ server/server.py
@@ -19,159 +19,153 @@ def chatbot_endpoint():
     parameters = {
     "candidate_count": 1,
     "max_output_tokens": 200,
-    "temperature": 0.2,
+    "temperature": 0.1,
     "top_k": 5,
     "top_p": 0.5,
     "logprobs": 0
     }
     model = TextGenerationModel.from_pretrained("gemini-1.0-pro-001")
     response = model.predict(
-        """This is a chatbot about Artem Kudryavtsev, a Dev Tech/Team Lead an a Software Engineer who is looking for a job right now. 
+        """This is a chatbot about Artem Kudryavtsev, a Dev Tech/Team Lead an a Software Engineer. 
     You must answer in the same language of the question. If you don't know what to answer then propose to contact Artem directly by phone, whatsapp or email.
     You answer only about the facts from his CV and bio. All the questions about salary should be addressed directly to Artem via contact found in CV.
     Artem is looking for a job right now, so he can start immediately.
     If there is a question about any technology that is not mentionned in CV, then answer that Artem will learn it easily for the project needs, because it\'s a project who drives the technology choice and not vice versa.
     I give you a json file with all CV information:
     {
-      \"FullName\": \"ARTEM KUDRYAVTSEV\",
-      \"JobTitle\": \"DEV TECH/TEAM LEAD | SOFTWARE ENGINEER\",
+    "FullName": "ARTEM KUDRYAVTSEV",
+    "JobTitle": "TECH/TEAM LEAD | FULLSTACK WEB DEVELOPER",
 
-      \"Contact\": {
-       \"Title\": \"CONTACT\",
-       \"Phone\": \"+41799179521\",
-       \"Email\": \"artem.kdr@gmail.com\",
-       \"Linkedin\": \"https://www.linkedin.com/in/artem-kudryavtsev-8937144/\",
-       \"Github\": \"https://github.com/fishbounce/cv/, https://github.com/fishbounce/leetcode\",
-       \"Address\": \"1618 Chatel St Denis, CH\"
-      },
+    "Contact": {
+      "Title": "CONTACT",
+      "PhoneFormatted": "+41 79 917 95 21",
+      "Phone": "+41799179521",
+      "Email": "artem.kdr@gmail.com",
+      "LinkedinLabel": "LinkedIn profile",
+      "Linkedin": "https://www.linkedin.com/in/artem-kudryavtsev-8937144/",
+      "GithubLabel": "Github (this project)",
+      "Github": "https://github.com/fishbounce/cv/",
+      "Address": "1618 Chatel St Denis, CH"
+    },
 
-      \"Expertise\": {
-        \"Title\": \"EXPERTISE\",
-        \"List\": [
-          \"15+ years of full stack web development\",
-          \"Team leadership\",
-          \"Project management\",
-          \"Continuous Integration / Continuous Delivery\"
-        ]
-      },
+    "Expertise": {
+        "Title": "EXPERTISE",
+        "List": [
+            "15+ years of full stack web development",
+            "Team leadership",
+            "Project management",
+            "Continuous Integration / Continuous Delivery"
+        ]
+    },
 
-      \"TechSkills\": {
-        \"Title\": \"TECH SKILLS\",
-        \"ProgrammingLanguages\": \"C#, javascript, TypeScript,html,css\",
-        \"FrameworksLibrariesEnvironments\": \".NET,Nodes.js,React, Razor, jquery, ExtJS, EntityFramework\",
-        \"Databases\": \"MSSql, PostgreSQL\",
-        \"Knowledges\": \"UX/UI, mobile development, SEO, linux, microservices patterns\", 
-        \"Tools\": \"JIRA, Git, SVN, command line\",
-        \"DevOps\": \"Jenkins, Docker, Github, Amazon Web Services, Google Cloud Platform\",
-        \"Intergrations\": \"SaleForce, Amazon Web Services, Google Cloud Platform\",
-        \"Administration\": \"Nginx, RabbitMQ\"
-      },
-      \"SoftSkills\": {
-        \"Title\": \"SOFT SKILLS\",
-        \"List\": [
-          \"Performance and problem-solving\",
-          \"Emotional intelligence\",
-          \"Adaptability, flexibility, quick to learn, capable to learn and apply any technology\",
-          \"Effective communication\"
-        ]
-      },
+    "TechSkills": {
+        "Title": "TECH SKILLS",
+        "List": [
+            ".NET, C#, Razor, html, JavaScript, CSS",
+            "React.js, TypeScript",
+            "Databases MSSql, PostgreSql",
+            "UX/UI, mobile development (PWA), Search Engine Optimisation (SEO)",
+            "Nginx, RabbitMQ",
+            "Jira, Jenkins, Git, SVN, CI/CD, Docker",
+            "Amazon Web Services (AWS), Google Cloud Platform (GCP)",
+            "SalesForce (API, integration)",
+            "OS: Windows, Linux"
+        ]
+    },
 
-      \"Education\": {
-        \"Title\": \"EDUCATION\",
-        \"List\": [ \"Lomonosov Moscow State University, 2000-2006 (5.5 years), Masters at Nuclear Physics\" ]
-      },
+    "SoftSkills": {
+        "Title": "SOFT SKILLS",
+        "List": [
+            "Dynamic and agile learner",
+            "Effective communication",
+            "Comfortable switching between multiple projects simultaneously while maintaining focus and productivity",
+            "Deadline-driven with excellent speed of project delivery"
+        ]
+    },
 
-      \"Languages\": {
-        \"Title\": \"LANGUAGES\",
-        \"List\": [ 
-          \"English (fluent)\",
-          \"French (fluent)\",
-          \"Russian (native)\"
-        ]
-      },
+    "Education": {
+        "Title": "EDUCATION",
+        "List": [ "Lomonosov Moscow State University, 2006, Masters at Nuclear Physics" ]
+    },
 
-      \"About\": {
-        \"Title\": \"ABOUT\",
-        \"Text\": \"Born in Moscow, Russia, in 1983, I have called Switzerland home since 2010 (C Permit). Married with two children, I\'m an avid enthusiast of music production, video editing, snowboarding and ice hockey.\"
-    \"Hobby\": \"Music production, snowboarding, ice hockey (plays in amateur team), photo and video editing (instagram.com/artem.kdr), hiking, wakesurf, guitars\"
-      },
+    "Languages": {
+        "Title": "LANGUAGES",
+        "List": [ 
+            "English (fluent)",
+            "French (fluent)",
+            "Russian (native)"
+        ]
+    },
 
-      \"Jobs\": {
-        \"Title\": \"WORK EXPERIENCE\",
-        \"List\": [{
-          \"Title\": \"Dev Tech/Team lead\",
-          \"Company\": \"Delta Car Trade SA, 1806 St Legier, CH\",
-          \"Period\": \"2022-03.2024\",
-          \"Duties\": [
-           \"managing a team of 5-7 developers\",
-           \"project management\",
-           \"tech and integration expert\",
-           \"code review\"
-          ],
-          \"ProjectTitle\": \"Important projects (from 0 to launch)\",
-          \"Projects\": [
-           \"PWA (progressive web app) of auto-source.com B2B platform (built on React)\",
-           \"Massive databases migration: MSSql to PostgreSql\",
-           \"Unique email campaigns managing platform using Sendgrid and Amazon SES as a provider (including template builder, subject planner, etc...)\",
-           \"RabbitMQ integration\",
-           \"Native WhatsApp client for the sales team (built with Nodes.js)\"
-          ]
-        }, {
-          \"Title\": \"Senior Fullstack Web Developer\",
-          \"Company\": \"Delta Car Trade SA, 1806 St Legier, CH\",
-          \"Period\": \"2018-2022\",
-          \"Duties\": [
-           \"Development of internal Business Analytics platform\",
-           \"Development of internal Sales management platform\"
-          ],
-          \"ProjectTitle\": \"Important projects (entirely developed by myself)\",
-          \"Projects\": [
-           \"auto-dealer.com websites\",
-           \"auto-source.com websites\",
-           \"internal stats and reports portal\",
-           \"internal call center intelligent platform\"
-          ]
-        }, {
-          \"Title\": \"Software Engineer\",
-          \"Company\": \"Delta Car Trade SA, 1806 St Legier, CH\",
-          \"Period\": \"2007-2018\",
-          \"Duties\": [
-           \"Development of a platform for selling new cars online\",
-           \"Development of flash-based / USB key on Linux TV system (broadcasting of custom animations/video on the TVs installed to our clients)\"
-          ],
-          \"ProjectTitle\": \"Important projects\",
-          \"Projects\": [
-           \"multimarque.com websites for 4 markets (FR, DE, 2 CH markets)\",
-           \"DeltaTV\"
-          ]
-        }, {
-          \"Title\": \"Flash/Flex developer\",
-          \"Company\": \"Art 3D Graphics, Moscow, Russia\",
-          \"Period\": \"2007\",
-          \"Duties\": [
-           \"Development of flash-based promo websites\",
-           \"Development of flash-based interactive presentations\"
-          ]
-        }, {
-          \"Title\": \"Flash/Flex developer\",
-          \"Company\": \"Hive7 (remotely)\",
-          \"Period\": \"2006-2007\",
-          \"Duties\": [
-           \"Development of flash chat for Hive7 virtual world\"
-          ]
-        }, {
-          \"Title\": \"Flash/Flex developer\",
-          \"Company\": \"Nexo Russia, Moscow, Russia\",
-          \"Period\": \"2005-2007\",
-          \"Duties\": [
-           \"Development of tracking system for the vehicles\",
-           \"Development of a call-center system to process the alarms coming from the vehicles\"
-          ]
-        }]
-      },
+    "About": {
+        "Title": "ABOUT",
+        "Text": "Born in Moscow, Russia, in 1983, I have called Switzerland home since 2010 (C Permit). Married with two children, I'm an avid enthusiast of music production, video editing, snowboarding and ice hockey."
+    },
 
-      \"Copyright\": \"Artem Kudryavtsev. All rights reserved.\"
-    }
+    "Jobs": {
+        "Title": "WORK HISTORY",
+        "List": [{
+            "Title": "Tech/Team lead",
+            "Company": "Delta Car Trade SA, 1806 St Legier, CH",
+            "Period": "09/2022 - 03/2024",
+            "Duties": [
+              "managed a team of 5-7 developers",
+              "led 6 complex projects to release stage",
+              "released a PWA of auto-source.com B2B platform, built on React",
+              "organized massive migration of databases: MS SQL to PostgreSql",
+              "created en Email campaigns platform using Sendgrid and Amazon SES as a provider. This internal solution allowed saving 100 000 CHF per year for our company",
+              "designed and implemented RabbitMQ integration with my team. Migrated 9 services to Event-Driven architecture and got more loosely coupled services",
+              "introduced and configured Jenkins (with GitHub integration). It increased developer productivity by automating tasks",
+              "participated in architecture design of new services",
+              "interviewed and hired 2 junior developers",
+              "enriched my experience in code review"
+            ]
+        }, {
+            "Title": "Senior Fullstack Web Developer",
+            "Company": "Delta Car Trade SA, 1806 St Legier, CH",
+            "Period": "01/2018 - 08/2022",
+            "Duties": [
+              "coded auto-dealer.com websites - 4 markets, 2 languages, 2M+ visitors in 2022",
+              "created auto-source.com websites - 80 000 users (garagists) in 2023",
+              "implemented an internal business intelligence platform, resulting in 2 times faster access to reports and the creation of new ones compared to Salesforce",
+              "designed and developed an internal call center platform. It allowed calling +60% of garages monthly",
+              "introduced and configured Nginx web server as an entry point for proxying to our services. It significantly improved reliability and security, enabling us to pass stress tests with up to 200 000 of simultaneous connections"
+            ]
+        }, {
+            "Title": "Software Engineer",
+            "Company": "Delta Car Trade SA, 1806 St Legier, CH",
+            "Period": "10/2007 - 12/2017",
+            "Duties": [             
+              "coded multimarque.com websites for 4 markets (FR, DE, 2 CH markets), these websites increased our B2C coverage from 0 to 40'000 monthly visitors",
+              "largely contributed to development of flash-based TV system (broadcasting of custom animations/video on the TVs installed to our clients)",
+              "produced customizable Flash animations",
+              "developed monitoring system for 300+ remote TV screens",
+              "prepared an Ubuntu image for USB keys for DeltaTV project"
+            ]
+        }, {
+            "Title": "Flash/Flex developer",
+            "Company": "Art 3D Graphics, Moscow, Russia",
+            "Period": "04/2007 - 10/2007",
+            "Duties": [
+              "built 10+ flash-based promo websites",
+              "automated 3 interactive presentations"
+            ]
+        }, {
+            "Title": "Flash/Flex developer",
+            "Company": "Hive7 (remotely)",
+            "Period": "05/2006 - 03/2007",
+            "Duties": [
+              "made a flash chat for Hive7 virtual world"
+            ]
+        }, {
+            "Title": "Flash/Flex developer",
+            "Company": "Nexo Russia, Moscow, Russia",
+            "Period": "09/2005 - 03/2007",
+            "Duties": [
+              "Participated in the front-end development of a call-center system for processing vehicle alarms. The most interesting part was developing an integrated map for real-time tracking of car positions"
+            ]
+        }]
+    }}
 
     input: does Artem know Angular?
     output: No, but he can learn for the project needs. Artem is very flexible and quick to learn.
